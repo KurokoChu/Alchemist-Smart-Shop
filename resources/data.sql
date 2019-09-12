2fa7ca6a7b4f7eff29dc32a3acfab4c6db283955
KurokoChu <ktutora@gmail.com>
2019-09-12 21:59:46 +0900

    Add data.sql

--- resources/data.sql
+++ resources/data.sql
@@ -1,5 +1,22 @@
-insert into potion
-values(10001);
-
-insert into potion
-values(10002);
+INSERT INTO "POTION" VALUES
+(10001, 0),
+(10002, 1),
+(10003, 1),
+(10004, 2),
+(10005, 0),
+(10006, 1),
+(10007, 2),
+(10008, 0),
+(10009, 0);                  
+INSERT INTO "HEALING_POTION" VALUES
+(60, 'A potion that is used to recover HP.', 'https://elwiki.net/wiki/images/3/35/HP_Potion_Beginner.png', 200.0, 'Beginner', 1000, 10001),
+(60, 'A potion that is used to recover HP.', 'https://elwiki.net/wiki/images/3/35/HP_Potion_Beginner.png', 200.0, 'Beginner', 1000, 10005),
+(60, 'A potion that is used to recover HP.', 'https://elwiki.net/wiki/images/3/35/HP_Potion_Beginner.png', 200.0, 'Beginner', 1000, 10008),
+(60, 'A potion that is used to recover HP.', 'https://elwiki.net/wiki/images/3/35/HP_Potion_Beginner.png', 200.0, 'Beginner', 1000, 10009);          
+INSERT INTO "RESURRECTION_POTION" VALUES
+(3, 60, 'A mysterious potion giving vitality to a allies so that they can revive.', 'https://elwiki.net/wiki/images/8/81/HQ_Shop_Item_78230.png', 8000.0, 'Elite', 10002),
+(3, 60, 'A mysterious potion giving vitality to a allies so that they can revive.', 'https://elwiki.net/wiki/images/8/81/HQ_Shop_Item_78230.png', 8000.0, 'Elite', 10003),
+(3, 60, 'A mysterious potion giving vitality to a allies so that they can revive.', 'https://elwiki.net/wiki/images/8/81/HQ_Shop_Item_78230.png', 8000.0, 'Elite', 10006);  
+INSERT INTO "STRENGTH_POTION" VALUES
+('Mysterious potion which grants the user the magical attack power.', 1800, 'Magical Attack +15%', 'https://elwiki.net/wiki/images/3/3a/HQ_Shop_Item_78550.png', 4000.0, 'Magical Attack', 10004),
+('Mysterious potion which grants the user the magical attack power.', 1800, 'Magical Attack +15%', 'https://elwiki.net/wiki/images/3/3a/HQ_Shop_Item_78550.png', 4000.0, 'Magical Attack', 10007);               
